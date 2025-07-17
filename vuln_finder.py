685056fa794047c392086325c0c6fea84983e2fc
k2xploit <k2xploit@proton.me>
2025-07-17 13:33:09 +0000

    added official github repo reference vuln_finder.py
    added official github repo reference

--- vuln_finder.py
+++ vuln_finder.py
@@ -2,6 +2,7 @@
 # -----------------------------------------------------------------------------
 #  vuln_finder.py - CVE & Vulnerability lookup tool
 #  Copyright (C) 2025  k2xploit
+#  Official github repo: https://github.com/k2xploit/vuln-finder
 #
 #  This program is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
